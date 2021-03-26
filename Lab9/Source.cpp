9623d6cbdc4b5d6245384c76ad2254e0aaa62210
jhmny <58868135+jhmny@users.noreply.github.com>
2021-03-25 21:05:51 -0700

    Copied Code from pdf

--- Lab9/Source.cpp
+++ Lab9/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+//#include "trieST.h"
+#include "Node.h"
 using namespace std;
 
 int main() {
---
+++ Lab9/trieST.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef STRING_ST_H
+#define STRING_ST_H
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <queue>
+
+using namespace std;
+
+template<class Value, class NodeType, Value(*nullValue)(), const int R = 256>
+class trieST {
+private:
+	shared_ptr<NodeType> root;
+public:
+	trieST() {
+		root = static_cast<shared_ptr<NodeType>>(new NodeType); 
+	}
+	void insert(string key, Value val);
+	bool contains(string key);
+	Value get(string key);
+	queue<pair<string, Value>> pairs();
+private:
+	shared_ptr<NodeType> insert(shared_ptr<NodeType> x, string key, Value val, int depth);
+	shared_ptr<NodeType> get(shared_ptr<NodeType> x, string key, int depth);
+	void pairs(shared_ptr<NodeType> x, string strKey, shared_ptr<queue<pair<string, value>>> queueOfPairs);
+};
+#endif // !STRING_ST_H
