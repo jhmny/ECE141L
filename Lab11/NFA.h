26997c961039c2e0262efa18a792cdedac869ea2
jhmny <58868135+jhmny@users.noreply.github.com>
2021-04-16 12:53:39 -0700

    regex incomplete as well as grep

--- Lab11/NFA.h
+++ Lab11/NFA.h
@@ -15,7 +15,7 @@ public:
     {
         M = regexp.length();
         re = regexp;
-        G = buildEpsilonTransitionDigraph();
+        G = buildEpsilonTransitionDigraph(re);
     }
 
     void print(Digraph G)
@@ -67,6 +67,42 @@ public:
             }
             if (re[i] == '(' || re[i] == '*' || re[i] == ')')
                 G.add_edge(Edge(i, i + 1));
+        }
+        return G;
+    }
+
+    Digraph buildEpsilonTransitionDigraph(string re) {
+        int M = re.length();
+        Digraph G(M + 1);
+        stack<int> ops;
+        Edge e;
+        for (int i = 0; i < M; i++) {
+            int lp = i;
+            if (re[i] == '(' || re[i] == '|')
+                ops.push(i);
+            else if (re[i] == ')') {
+                int pos = ops.top();
+                ops.pop();
+                if (re[pos] == '|') {
+                    lp = ops.top();
+                    ops.pop();
+                    e.update(lp, pos + 1);
+                    G.add_edge(e);
+                    e.update(pos, i);
+                    G.add_edge(e);
+                }
+                else lp = pos;
+            }
+            if (i < M - 1 && re[i + 1] == '*') {
+                e.update(lp, i + 1);
+                G.add_edge(e);
+                e.update(i + 1, lp);
+                G.add_edge(e);
+            }
+            if (re[i] == '(' || re[i] == '*' || re[i] == ')') {
+                e.update(i, i + 1);
+                G.add_edge(e);
+            }
         }
         return G;
     }
